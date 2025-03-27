0802d9dcdb9d603b848665844a7b823aff272e5b
piy-bebe <piy.bebe@gmail.com>
2025-03-27 15:27:36 +0300

    WIP: добавить topbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import { Categories, Container, Title } from '@/components/shared'
+import { Container, Title } from '@/components/shared'
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
-
-        <Categories />
       </Container>
     </>
   )
